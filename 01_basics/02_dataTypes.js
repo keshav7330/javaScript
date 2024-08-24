ee69d526e14c38fb7cb85c4f8c5ced93086eb2e5
keshav7330 <84995893+keshav7330@users.noreply.github.com>
2024-08-24 17:38:44 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,25 @@
 "use strict" //treat all js code as newer version
 
-alert(3 + 3) //
+//alert(3 + 3) //we are using nodejs, not browser.
+
+console.log(3 + 3)
+
+console.log("keshav");
+
+
+let name = "keshav"
+let age = 21
+let isLoggedIn = false
+
+// number =  2 to power 53
+// bigint 
+// string = " "
+// boolean
+// null = stand alone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
