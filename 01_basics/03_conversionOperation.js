4c672660602766bf85cc640b16493cf51a6c750e
keshav7330 <84995893+keshav7330@users.noreply.github.com>
2024-08-26 21:01:52 +0000

    comparison operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,10 @@
 let score = null
-
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-
-console.log(typeof (valueInNumber));
-
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 
 //  "33" => 33
@@ -17,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "hello"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false 
 //  "" => false
@@ -26,5 +23,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNum = String(someNumber)
-console.log( typeof stringNum);
-console.log(stringNum);
+// console.log( typeof stringNum);
+// console.log(stringNum);
+
+// ********* operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" +2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 +"2" + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+let hello = gameCounter++;
+console.log(gameCounter);
+console.log(hello);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(true > 0);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// Comparison and equality check works differently
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+
+
+// ===
+
+console.log("2" == 2);
+console.log("2" === 2);
+
