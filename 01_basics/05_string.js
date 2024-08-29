3b46d1b9cee2a6c8d563c86276ce3d7cd1adebf0
keshav7330 <84995893+keshav7330@users.noreply.github.com>
2024-08-29 19:01:29 +0000

    numAndMaths

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,9 +3,9 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hello')
+const gameName = new String('hello-world-hw')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,23 +13,25 @@ const gameName = new String('hello')
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('o'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('o'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-3, 4)
 console.log(anotherString);
 
 const newStringOne = "   keshav    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://keshav.com/keshav%20chettry"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '_'))
 
-console.log(url.includes('sundar'))
+// console.log(url.includes("keshav"));
 
-console.log(gameName.split('-'));
+console.log(gameName.split("-"));
+
+ 
---
+++ 01_basics/06_numsMaths.js
@@ -0,0 +1,42 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(100.0);
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.78864
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//********************** Maths *********************** */
+
+let hello = -4.66666
+
+// console.log(Math.abs(hello));
+// console.log(Math.round(hello));
+// console.log(Math.ceil(hello));
+// console.log(Math.floor(hello));
+// console.log(Math.min(5,6,1,8,0));
+// console.log(Math.max(5,6,1,8,0));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 6)  + 1 );
+
+const min = 10
+const max = 20
+
+console.log((Math.round(Math.random() * min)) + min);
+
+console.log(Math.round(Math.random() * min));
+
+
+
